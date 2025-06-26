5589108792c7505e8b1b48fd3cc188da5fdfc4e1
baxti0331 <sheraliyevbaxtiyor3103@gmail.com>
2025-06-26 17:18:08 +0300

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -7,14 +7,22 @@ app = Flask(__name__)
 
 @app.route("/api/bot", methods=["POST"])
 def webhook():
-    update = telebot.types.Update.de_json(request.get_json())
-    bot.process_new_updates([update])
+    update = telebot.types.Update.de_json(request.get_json(force=True))
+    if update.message:
+        handle_message(update.message)
     return "OK", 200
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    web_app_info = telebot.types.WebAppInfo(url="https://x-0-pi.vercel.app/")
-    button = telebot.types.InlineKeyboardButton(text="PLAY🕹️", web_app=web_app_info)
-    markup.add(button)
-    bot.send_message(message.chat.id, "Привет! Нажми Кнопку чтобы открыть приложение.", reply_markup=markup)
+@app.route("/api/bot", methods=["GET"])
+def check():
+    return "Webhook OK", 200
+
+def handle_message(message):
+    if message.text == "/start":
+        markup = telebot.types.InlineKeyboardMarkup()
+        web_app_info = telebot.types.WebAppInfo(url="https://x-0-pi.vercel.app/")
+        button = telebot.types.InlineKeyboardButton(text="PLAY 🕹️", web_app=web_app_info)
+        markup.add(button)
+        
+        bot.send_message(message.chat.id, "Привет! Нажми кнопку, чтобы открыть приложение.", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, f"Ты написал: {message.text}")
